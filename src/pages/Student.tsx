be3805d449ba27891e3d9a950a9c25a8a2e46090
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-03-13 21:40:54 +0700

    lab1

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,48 +1,62 @@
-import React, { useState } from 'react'
-import Table from 'react-bootstrap/Table'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './student.css'
-const Student = () => {
-    const [data, setData] = useState(
-        [
-            { id: 1, name: 'Nguyễn Văn Linh', age: 21, phone: '0369053052', email: 'linhnvph13162@fpt.edu.vn', avatar: 'https://picsum.photos/50/50' },
-            { id: 2, name: 'Nguyễn Đức Hải', age: 22, phone: '0968856903', email: 'haindph13158@fpt.edu.vn', avatar: 'https://picsum.photos/50/50' },
-            { id: 3, name: 'Trương Mạnh Dũng', age: 22, phone: '098888888', email: 'dungtmph12934@fpt.edu.vn', avatar: 'https://picsum.photos/50/50' },
-        ]
-    )
-    const removeProduct = (id: number) => {
-        setData(data.filter(item => item.id !== id))
-    }
+import React from 'react'
+
+function Student() {
+    const students = [
+        {
+            id: 1,
+            name: 'Trinh Minh Tuan',
+            age: 21,
+            phone: '0348090652',
+            email: 'tuantmph13096@fpt.edu.vn',
+            avatar: 'http://placeimg.com/300/200/sports'
+        },
+        {
+            id: 2,
+            name: 'Tran Minh Dung',
+            age: 20,
+            phone: '0123456789',
+            email: 'dungtm@fpt.edu.vn',
+            avatar: 'http://placeimg.com/300/200/sports'
+        },
+        {
+            id: 3,
+            name: 'Nguyen Van Linh',
+            age: 21,
+            phone: '0987654321',
+            email: 'linhnv@fpt.edu.vn',
+            avatar: 'http://placeimg.com/300/200/sports'
+        },
+    ]
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>STT</th>
-                    <th>Tên</th>
-                    <th>Tuổi</th>
-                    <th>SDT</th>
-                    <th>Email</th>
-                    <th>Avatar</th>
-                    <th>Hành động</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) =>
-                    <tr key={index}>
-                        <td style={{verticalAlign:"middle"}}>{index + 1}</td>
-                        <td style={{verticalAlign:"middle"}}>{item.name}</td>
-                        <td style={{verticalAlign:"middle"}}>{item.age}</td>
-                        <td style={{verticalAlign:"middle"}}>{item.phone}</td>
-                        <td style={{verticalAlign:"middle"}}>{item.email}</td>
-                        <td style={{verticalAlign:"middle"}}>
-                            <img src={item.avatar} alt="" />
-                        </td>
-                        <td><button className='btn btn-danger' onClick={() => removeProduct(item.id)}>Xóa</button></td>
+        <div className='container'>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">STT</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Age</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Avatar</th>
+                        <th scope="col">Action</th>
                     </tr>
-                )}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {students.map(item => (
+                        <tr>
+                            <th scope="row">{item.id} </th>
+                            <td>{item.name}</td>
+                            <td>{item.age}</td>
+                            <td>{item.phone}</td>
+                            <td>{item.email}</td>
+                            <td><img src={item.avatar} alt={item.avatar} /></td>
+                            <td><button className="btn btn-danger">Xóa</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
-export default Student
+export default Student 
