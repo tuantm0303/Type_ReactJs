ac4742b40680f848e0c69b740d1e48af36004c2f
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-03-14 16:51:33 +0700

    remove item

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,32 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Student() {
-    const students = [
+    const [students, setStudents] = useState([
         {
-            id: 1,
-            name: 'Trinh Minh Tuan',
-            age: 21,
-            phone: '0348090652',
-            email: 'tuantmph13096@fpt.edu.vn',
-            avatar: 'http://placeimg.com/300/200/sports'
+            id: 1, name: 'Trịnh Minh Tuấn', age: 21, phone: '0348090652', email: 'tuantmph13096@fpt.edu.vn', avatar: 'http://placeimg.com/300/200/sports'
         },
         {
-            id: 2,
-            name: 'Tran Minh Dung',
-            age: 20,
-            phone: '0123456789',
-            email: 'dungtm@fpt.edu.vn',
-            avatar: 'http://placeimg.com/300/200/sports'
+            id: 2, name: 'Trần Minh Dũng', age: 19, phone: '0123456789', email: 'dungtm@fpt.edu.vn', avatar: 'http://placeimg.com/300/200/sports'
         },
         {
-            id: 3,
-            name: 'Nguyen Van Linh',
-            age: 21,
-            phone: '0987654321',
-            email: 'linhnv@fpt.edu.vn',
-            avatar: 'http://placeimg.com/300/200/sports'
+            id: 3, name: 'Nguyễn Văn Linh', age: 20, phone: '0987654321', email: 'linhnv@fpt.edu.vn', avatar: 'http://placeimg.com/300/200/sports'
         },
-    ]
+    ])
+
+    const handleRemove = (id: number) => {
+        const confirm = window.confirm('Are you sure delete??')
+        if(confirm) {
+            setStudents(students.filter(student => student.id !== id))
+        }
+    }
+
     return (
         <div className='container'>
             <table className="table">
@@ -42,15 +35,15 @@ function Student() {
                     </tr>
                 </thead>
                 <tbody>
-                    {students.map(item => (
+                    {students.map((item, index) => (
                         <tr>
-                            <th scope="row">{item.id} </th>
+                            <th scope="row" key={index}>{item.id} </th>
                             <td>{item.name}</td>
                             <td>{item.age}</td>
                             <td>{item.phone}</td>
                             <td>{item.email}</td>
                             <td><img src={item.avatar} alt={item.avatar} /></td>
-                            <td><button className="btn btn-danger">Xóa</button></td>
+                            <td><button className="btn btn-danger" onClick={() => handleRemove(item.id)}>Xóa</button></td>
                         </tr>
                     ))}
                 </tbody>
