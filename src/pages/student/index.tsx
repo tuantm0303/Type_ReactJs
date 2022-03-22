3831d021b0b3051ce46262559358b767d52a2c91
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-03-23 01:08:14 +0700

    style: tailwind css

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,38 +1,60 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
-type row = {
+type student = {
   name: string;
   age: string;
   phone: string;
 };
 
 type TableProps = {
-  rows: row[];
+  students: student[];
 };
 
-function Student({ rows }: TableProps) {
+function Student({ students }: TableProps) {
   return (
-    <div className='container'>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Age</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((item, index) => (
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.age}</td>
-              <td>{item.phone}</td>
-              <td><button className="btn btn-danger">Xóa</button></td>
+    <div className="max-w-2xl mx-auto">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="p-4">
+        </div>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Age
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Phone
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {students.map((item, index) => (
+              <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                  {item.name}
+                </th>
+                <td className="px-6 py-4">
+                  {item.age}
+                </td>
+                <td className="px-6 py-4">
+                  {item.phone}
+                </td>
+                <td className="px-6 py-4 text-right">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
     </div>
   );
 }
