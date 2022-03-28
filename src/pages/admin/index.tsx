cf430782e33824f66594124e2ad6d134cb385bda
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-03-29 00:17:28 +0700

    update: nav link

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,13 +9,13 @@ function Admin() {
         <aside className="w-80 bg-gray w-fulll hidden sm:block">
           <div className="flex flex-col justify-between h-full p-4 bg-gray-800">
             <div className="text-sm">
-              <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300"><Link to="post">Post</Link></div>
-              <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300"><Link to="product">Product</Link></div>
-              {/* <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300"><Link to="category">Category</Link></div>
-              <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300"><Link to="user">User</Link></div> */}
+              <Link to="post"><div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Post</div></Link>
+              <Link to="product"><div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Product</div></Link>
+              {/* <Link to="category"><div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Category</div></Link>
+              <Link to="user"><div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">User</div></Link> */}
             </div>
 
-            <div className="flex p-3 text-white bg-red-500 rounded cursor-pointer text-center text-sm">
+            <div className="flex p-3 text-white bg-red-500 rounded cursor-pointer text-center text-sm mt-2">
               <button className="rounded inline-flex items-center">
                 <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" /></svg>
                 <span className="font-semibold">Logout</span>
