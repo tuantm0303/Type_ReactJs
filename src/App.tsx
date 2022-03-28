4223cbe7c8f7af445f22271b821fdaadf6ef9846
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-03-28 22:18:59 +0700

    style: validate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import PostOutlet from "./pages/posts/PostOutliet";
 import ProductAdmin from "./pages/products";
+import PostAdmin from "./pages/posts";
 import AddProduct from "./pages/products/addProduct";
+import AddPost from "./pages/posts/addPost";
 import ProductDetail from "./pages/products/productDetail";
 import ProductOutlet from "./pages/products/ProductOutlet";
+import PostDetail from "./pages/posts/PostDetail";
+import EditProduct from "./pages/products/editProduct";
 
 function App() {
   return (
@@ -12,19 +17,27 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/product">Product</Link></li>
+          <li><Link to="/post">Post</Link></li>
         </ul>
       </div>
+      {/* product */}
       <Routes>
-        <Route path="product/" element={<ProductOutlet />}>          
+        <Route path="product" element={<ProductOutlet />}>
           <Route index element={<ProductAdmin />} />
           <Route path="add" element={<AddProduct />} />
-          <Route path="edit/:id" element={<AddProduct />} />
+          <Route path="edit/:id" element={<EditProduct />} />
           <Route path=":id" element={<ProductDetail />} />
         </Route>
+
+        {/* post */}
+        <Route path="post" element={<PostOutlet />}>
+          <Route index element={<PostAdmin />} />
+          <Route path="add" element={<AddPost />} />
+          {/* <Route path="edit/:id" element={<EditPost />} /> */}
+          <Route path=":id" element={<PostDetail />} />
+        </Route>
       </Routes>
-      <header className="App-header">
-        
-      </header>
+
     </div>
   );
 }
--- src/pages/products/editProduct.tsx
+++ src/pages/products/editProduct.tsx
@@ -40,8 +40,8 @@ function EditProduct() {
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
             {...register("name", { required: true })}
           />
-        </div>
         {errors.name && <span style={{ color: "red" }}>This field is required</span>}
+        </div>
         <div className="relative z-0 mb-6 w-full group">
           <input
             type="number"
@@ -49,8 +49,8 @@ function EditProduct() {
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
             {...register("price", { required: true, valueAsNumber: true })} //valueAsNumber convert number
           />
-        </div>
         {errors.price && <span style={{ color: "red" }}>This field is required</span>}
+        </div>
         <button
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
