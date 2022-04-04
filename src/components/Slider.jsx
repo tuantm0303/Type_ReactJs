50d767645dcbe247d5dbe8ae08a99008afc1f732
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-04-04 18:04:18 +0700

    refactor: banner

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,39 +4,15 @@ import { Carousel } from 'react-carousel-minimal'
 function Slider() {
   const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      image: "https://cdn.tgdd.vn/2022/03/banner/Aseri-830-300-830x300-1.png",
       caption: ``
     },
     {
-      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      image: "https://cdn.tgdd.vn/2022/04/banner/830-300-830x300.png",
       caption: ""
     },
     {
-      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: ""
-    },
-    {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: ""
-    },
-    {
-      image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: ""
-    },
-    {
-      image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: ""
-    },
-    {
-      image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: ""
-    },
-    {
-      image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: ""
-    },
-    {
-      image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+      image: "https://cdn.tgdd.vn/2022/03/banner/reno7-830-300-830x300-3.png",
       caption: ""
     }
   ];
@@ -71,8 +47,6 @@ function Slider() {
             pauseIconSize="40px"
             slideBackgroundColor="darkgrey"
             slideImageFit="cover"
-            // thumbnails={true}
-            // thumbnailWidth="100px"
             style={{
               textAlign: "center",
               maxWidth: "1520px",
