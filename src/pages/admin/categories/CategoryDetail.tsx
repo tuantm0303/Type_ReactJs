c3c300e69a3110e9b468c7a8d5272cc335dc1602
tuantmph13096 <tuantmph13096@fpt.edu.vn>
2022-04-12 11:05:13 +0700

    style: detal admin

--- src/pages/admin/categories/CategoryDetail.tsx
+++ src/pages/admin/categories/CategoryDetail.tsx
@@ -20,14 +20,17 @@ function CategoryDetail(props: Props) {
   }, [])
 
   return (
-    <div className='bg-red-500 text-center'>
+    <>
+      <h2 className='text-3xl font-semibold text-center text-gray-800'>Detail Category</h2>
       <Link to='../' className='border-2 border-blue-500 float-right rounded-lg font-bold text-blue-500 px-3 py-2 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mb-5'>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
         </svg>
       </Link>
-      <h2>Name: {category?.name}</h2>
-    </div>
+      <div className="max-w-5xl grid grid-cols-2 gap-10 my-10">
+        <p className='text-2xl font-semibold text-gray-800'>Name: {category?.name}</p>
+      </div>
+    </>
   );
 }
 
